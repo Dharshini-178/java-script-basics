4dc6a7aa92108f78e17c3d4aacc0964c3047ad05
Dharshini Palanisamy <p.dharshinilogesh@gmail.com>
2025-09-19 21:46:16 +0530

    Update Task-2 arrow function programs with const

---
+++ Task-2/addNumbers.js
@@ -0,0 +1,2 @@
+const addNumbers = (a, b) => a + b;
+console.log("addNumbers(10, 20) =>", addNumbers(10, 20));
---
+++ Task-2/mapStringLengths.js
@@ -0,0 +1,5 @@
+const fruits = ["apple", "banana", "kiwi", "mango"];
+const lengths = fruits.map((word) => {
+    return word.length;
+});
+console.log("Lengths of words are:", lengths);
